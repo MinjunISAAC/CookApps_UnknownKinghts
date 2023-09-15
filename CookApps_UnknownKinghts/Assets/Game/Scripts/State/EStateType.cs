58407d88350cdc69ae19bbcc713e0ee2658b1bfc
Min Jun <qkralswns931026@gmail.com>
2023-09-15 20:38:32 +0900

    [DEV] 모작 State 추상화

---
+++ CookApps_UnknownKinghts/Assets/Game/Scripts/State/EStateType.cs
@@ -0,0 +1,12 @@
+namespace InGame.ForState
+{
+    public enum EStateType
+    {
+        Unknown     = 0,
+        Village     = 1,
+        BattleReady = 2,
+        BulidDeck   = 3,
+        Battle      = 4,
+        Result      = 5,
+    }
+}
---
+++ CookApps_UnknownKinghts/Assets/Game/Scripts/State/State_Village.cs
@@ -0,0 +1,54 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+// ----- User Defined
+using Utility.SimpleFSM;
+
+namespace InGame.ForState
+{
+    public class State_Village : SimpleState<EStateType>
+    {
+        // --------------------------------------------------
+        // Variables
+        // --------------------------------------------------
+        // ----- Owner
+        private Owner _owner = null;
+
+        // --------------------------------------------------
+        // Property
+        // --------------------------------------------------
+        public override EStateType State => EStateType.Village;
+
+        // --------------------------------------------------
+        // Functions - Nomal
+        // --------------------------------------------------
+        // ----- Protected Game Flow
+        protected override void _Start(EStateType preStateKey, object startParam)
+        {
+            Debug.Log($"<color=yellow>[State_{State}._Start] {State} State에 진입하였습니다.</color>");
+
+            #region <Manage Group>
+            _owner = Owner.NullableInstance;
+            if (_owner == null)
+            {
+                Debug.LogError($"<color=red>[State_{State}._Start] Owner가 Null 상태입니다.</color>");
+                return;
+            }
+
+            #endregion
+        }
+        protected override void _Update()
+        {
+
+        }
+
+        protected override void _Finish(EStateType nextStateKey)
+        {
+            Debug.Log($"<color=yellow>[State_{State}._Start] {State} State에 이탈하였습니다.</color>");
+        }
+    }
+}
