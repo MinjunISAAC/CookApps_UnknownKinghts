af7a284dcfce0cb0866e38ab65f9a1e4ce1472de
Min Jun <qkralswns931026@gmail.com>
2023-09-15 19:52:25 +0900

    [DEV] Owner 구조 구현

---
+++ CookApps_UnknownKinghts/Assets/Game/Scripts/Common/Owner.cs
@@ -0,0 +1,43 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace InGame
+{
+    public class Owner : MonoBehaviour
+    {
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+        //[Header("UI Group")]
+        //[SerializeField] private MainUI _mainUI = null;
+
+
+        // --------------------------------------------------
+        // Variables
+        // --------------------------------------------------
+
+        // --------------------------------------------------
+        // Properties
+        // --------------------------------------------------
+        public static Owner NullableInstance
+        {
+            get;
+            private set;
+        } = null;
+
+        // --------------------------------------------------
+        // Functions - Event
+        // --------------------------------------------------
+        private void Awake() { NullableInstance = this; }
+
+        private IEnumerator Start()
+        {
+
+            yield return null;
+        }
+    }
+}
